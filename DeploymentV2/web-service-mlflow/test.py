f8eb6ca4ab0065ba3e000b2083a92c6510ae7f41
jaybfn <jayesh.bfn@gmail.com>
2023-01-23 14:44:27 +0000

    updates

--- DeploymentV2/web-service-mlflow/test.py
+++ DeploymentV2/web-service-mlflow/test.py
@@ -2,10 +2,11 @@ import requests
 
 
 ride = {
-    "PULocationID":100,
+    "PULocationID":175,
     "DOLocationID": 15,
-    "trip_distance": 250
+    "trip_distance": 100
 }
+
 
 url = 'http://localhost:9696/predict'
 response = requests.post(url, json=ride)
